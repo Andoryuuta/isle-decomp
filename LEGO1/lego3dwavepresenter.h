f9a97ed68266aeb5834c8773d1f209afd9821d09
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2023-10-16 20:11:38 -0400

    Refine Lego3DWavePresenter

--- LEGO1/lego3dwavepresenter.h
+++ LEGO1/lego3dwavepresenter.h
@@ -1,13 +1,19 @@
 #ifndef LEGO3DWAVEPRESENTER_H
 #define LEGO3DWAVEPRESENTER_H
 
-#include "legowavepresenter.h"
+#include "mxwavepresenter.h"
 
 // VTABLE 0x100d52b0
 // SIZE 0xa0
-class Lego3DWavePresenter : public LegoWavePresenter
+class Lego3DWavePresenter : public MxWavePresenter
 {
 public:
+  // Inlined at 0x1000a217
+  inline Lego3DWavePresenter()
+  {
+    // TODO
+  }
+
   // OFFSET: LEGO1 0x1000d890
   inline virtual const char *ClassName() const override // vtable+0x0c
   {
