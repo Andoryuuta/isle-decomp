63fc064589c303ddc1cdeab894d81d4458ea8f72
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2023-10-16 20:12:34 -0400

    Add LegoAct2

---
+++ LEGO1/legoact2.h
@@ -0,0 +1,14 @@
+#ifndef LEGOACT2_H
+#define LEGOACT2_H
+
+#include "legoworld.h"
+
+// VTABLE 0x100d82e0
+// SIZE 0x1154
+class LegoAct2 : public LegoWorld
+{
+public:
+  LegoAct2();
+};
+
+#endif // LEGOACT2_H
